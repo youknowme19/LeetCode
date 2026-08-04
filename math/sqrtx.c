290e8a99173287ba2560ff0969a601deaae1a034
Sandeep <7006205280dk@gmail.com>
2026-08-04 22:43:25 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.c
+++ math/sqrtx.c
@@ -1,3 +1,21 @@
 int mySqrt(int x) {
-    return pow(x,0.5);
+    int low = 0;
+    int high = x;
+    int ans = 0;
+    
+    while (low <= high){
+       int mid = (low + high)/2;
+       int square = mid * mid;
+        
+        if(square == x)
+            return mid;
+        else if(square < x){
+            ans = mid; 
+            low = mid + 1;
+        }
+        else
+            high = mid - 1 ;
+    }      
+    return ans;
+
 }
