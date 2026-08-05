a456d451a87459115c74e0e743546649a8ffb0a0
Sandeep <7006205280dk@gmail.com>
2026-08-05 20:37:13 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.c
+++ memoization/climbing-stairs.c
@@ -1,8 +1,14 @@
 int climbStairs(int n) {
-    if(n<3){
+    if (n <= 2) {
         return n;
     }
-    int prev=n-1;
-    int prev1=prev-1;
-    return prev+prev1;
+    int prev2 = 1;
+    int prev1 = 2;
+    int current = 0;
+    for (int i = 3; i <= n; i++) {
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    return current;
 }
