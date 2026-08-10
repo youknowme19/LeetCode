5d243e091c08bd57dfa9e2b18f12eef3ccef421a
Sandeep <7006205280dk@gmail.com>
2026-08-10 20:57:10 +0530

    LC-stone-game-iv [Runtime: N/A; Memory: N/A]

--- zero-sum-game/stone-game-iv.c
+++ zero-sum-game/stone-game-iv.c
@@ -2,7 +2,9 @@ bool winnerSquareGame(int n) {
     if(n==1){
         return true;
     }
-   
+    else if(n%2!=0){
+        return true;
+    }
     int a = sqrt(n);
     if(a*a==n){
         return true;
