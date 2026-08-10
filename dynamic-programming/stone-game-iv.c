811666b657bd330bdc6ea1a345cf3e85ec927a45
Sandeep <7006205280dk@gmail.com>
2026-08-10 20:56:37 +0530

    LC-stone-game-iv [Runtime: N/A; Memory: N/A]

--- dynamic-programming/stone-game-iv.c
+++ dynamic-programming/stone-game-iv.c
@@ -2,9 +2,7 @@ bool winnerSquareGame(int n) {
     if(n==1){
         return true;
     }
-    else if(n%2!=0){
-        return false;
-    }
+   
     int a = sqrt(n);
     if(a*a==n){
         return true;
