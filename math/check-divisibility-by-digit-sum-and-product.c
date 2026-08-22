9319c16447fac0a24ffc8f53dc7bd32ab7842b07
Sandeep <7006205280dk@gmail.com>
2026-08-22 19:45:42 +0530

    LC-check-divisibility-by-digit-sum-and-product [Runtime: N/A; Memory: N/A]

--- math/check-divisibility-by-digit-sum-and-product.c
+++ math/check-divisibility-by-digit-sum-and-product.c
@@ -2,7 +2,7 @@ bool checkDivisibility(int n) {
     int sum=0;
     int product=1;
     int d=n;
-    while(d!=0){
+    while(d>0){
         int a=d%10;
         sum=sum+a;
         product=product*a;
